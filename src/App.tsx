53e961ec94ebf1fc2267e3329eacdac08470f49b
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-16 11:30:06 +0300

    added AddTaskForm, TaskList;

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import AddTaskForm from './components/AddTaskForm/AddTaskForm';
+import TaskList from './containers/ToDoTask/TaskList';
+
 const App = () => {
     return (
-    <>
-    Hello World!
-    </>
+    <div className="container">
+      <AddTaskForm />
+      <TaskList />
+    </div>
   )
 };
 
 export default App;
+
