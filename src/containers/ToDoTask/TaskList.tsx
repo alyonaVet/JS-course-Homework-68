5ee246902a658e60c74e079c3935374a737e0ea1
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-16 11:29:39 +0300

    created component TaskList;

---
+++ src/containers/ToDoTask/TaskList.tsx
@@ -0,0 +1,45 @@
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppDispatch, RootState} from '../../app/store';
+import {fetchTasks} from './ToDoTaskSlice';
+import Task from '../../components/Task/Task';
+
+const TaskList: React.FC = () => {
+  const tasks = useSelector((state: RootState) => state.todoTask.tasks);
+  const dispatch: AppDispatch = useDispatch();
+
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
+  const checkStatus = () => {
+
+  };
+
+  const deleteTask = () => {
+  };
+
+  return (
+    <>
+      <ul className="list-group">
+        {tasks && tasks.length > 0 ? (
+          tasks.map((task) => (
+            <Task
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              status={task.status}
+              onChecked={checkStatus}
+              onDelete={deleteTask}
+            />
+          ))
+        ) : (
+          <p className="container mt-5">No tasks yet</p>
+        )}
+      </ul>
+    </>
+  );
+};
+
+export default TaskList;
